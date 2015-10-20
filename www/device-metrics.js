1f1e4a49dae10ad5a8355dc381c710845dcf26a5
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2015-10-20 11:52:05 +0200

    Fix js api

--- www/device-metrics.js
+++ www/device-metrics.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     getDpi: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "DeviceDisplayMetrics", "getDpi");
+        cordova.exec(successCallback, errorCallback, "DeviceDisplayMetrics", "getDpi", []);
     }
 };
